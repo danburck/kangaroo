69fdfd96e4bd0d51e912e43606398bb103883659
danburck <daniel.m.burckhardt@gmail.com>
2021-03-29 17:40:16 +0200

    feat: add cocktail model and unit tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_142948) do
+ActiveRecord::Schema.define(version: 2021_03_29_151225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
+  create_table "cocktails", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cocktails_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -31,4 +39,6 @@ ActiveRecord::Schema.define(version: 2021_03_29_142948) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "cocktails", "users"
 end
