663e674abfb870fe10fc4ede184a10dc91d30b68
danburck <daniel.m.burckhardt@gmail.com>
2021-04-01 23:21:48 +0200

    Feat(Cloudinary): Add photo upload in Bartender page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @cocktail = Cocktail.new
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render user_path(@user)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:photo)
+  end
 end
