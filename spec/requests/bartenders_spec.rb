68fd4997df7c8f58d2e1f3a4fe1ebf914a3944e3
danburck <daniel.m.burckhardt@gmail.com>
2021-04-03 00:40:31 +0200

    delete unused tests

--- spec/requests/bartenders_spec.rb
+++ spec/requests/bartenders_spec.rb
@@ -1,46 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Bartenders", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/bartenders/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+RSpec.describe 'Bartenders', type: :request do
   describe "GET /create" do
     it "returns http success" do
-      get "/bartenders/create"
+      get bartenders_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /index" do
     it "returns http success" do
-      get "/bartenders/index"
+      get bartenders_path
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/bartenders/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/bartenders/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /delete" do
-    it "returns http success" do
-      get "/bartenders/delete"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
