8fe35f2ba87c68233b1c68af6f0426baa297076d
danburck <daniel.m.burckhardt@gmail.com>
2021-04-04 16:55:58 +0200

    Feat (API): add GET Bartenders#show

--- app/controllers/api/v1/bartenders_controller.rb
+++ app/controllers/api/v1/bartenders_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::BartendersController < Api::V1::BaseController
+  before_action :set_bartender, only: [:show]
+
   def index
     @bartenders = policy_scope(Bartender)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_bartender
+    @bartender = Bartender.find(params[:id])
+    authorize @bartender
   end
 end
--- app/views/api/v1/bartenders/index.json.jbuilder
+++ app/views/api/v1/bartenders/index.json.jbuilder
@@ -1,3 +1,3 @@
 json.array! @bartenders do |bartender|
-  json.extract! bartender, :id, :name, :address
+  json.extract! bartender, :id, :name, :address, :user_id, :latitude, :longitude
 end
